27679fb4d93c73d542a20098a0bb771e6d85dee6
Fares <fares_rdt@MacBook-Pro.local>
2020-06-28 21:30:20 +0200

    Premier fichier du site et premier test de git

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+    <title>Emmobilier</title>
+</head>
+
+<body>
+
+    <h1>Emmobilier</h1>
+
+</body>
+
+</html>
